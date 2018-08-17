dbf5bbcd445f39eedb396b478444ea7f4967be2d
Wizard <lsw1991abc@gmail.com>
2018-08-17 17:25:13 +0800

    升级镜像版本到1.11.2

--- README.md
+++ README.md
@@ -1,5 +1,22 @@
 # k8s-library
 
+## 2018-08-17
+切换到使用Aliyun的Mirror:  [https://dev.aliyun.com/list.html?namePrefix=google_container](https://dev
+.aliyun.com/list.html?namePrefix=google_container)
+
+- k8s.gcr.io/etcd-amd64:3.2.18
+- k8s.gcr.io/coredns:1.1.3
+- k8s.gcr.io/pause-amd64:3.1
+- k8s.gcr.io/pause:3.1
+- k8s.gcr.io/k8s-dns-kube-dns-amd64:1.14.10
+- k8s.gcr.io/k8s-dns-dnsmasq-nanny-amd64:1.14.10
+- k8s.gcr.io/k8s-dns-sidecar-amd64:1.14.10
+- k8s.gcr.io/kube-proxy-amd64:v1.11.2
+- k8s.gcr.io/kube-scheduler-amd64:v1.11.2
+- k8s.gcr.io/kube-controller-manager-amd64:v1.11.2
+- k8s.gcr.io/kube-apiserver-amd64:v1.11.2
+- k8s.gcr.io/kubernetes-dashboard-amd64:v1.8.3
+
 ## 2018-05-25
 - k8s.gcr.io/kube-proxy-amd64:v1.10.3             
 - k8s.gcr.io/kube-apiserver-amd64:v1.10.3         
--- pull.sh
+++ pull.sh
@@ -1,25 +1,23 @@
 #!/bin/bash
 
-images=( \
-	[0]="kube-proxy-amd64:v1.10.3" \
-	[1]="kube-controller-manager-amd64:v1.10.3" \
-	[2]="kube-apiserver-amd64:v1.10.3" \
-	[3]="kube-scheduler-amd64:v1.10.3" \
-	[4]="etcd-amd64:3.1.12" \
-	[5]="pause-amd64:3.1" \
-	[6]="k8s-dns-dnsmasq-nanny-amd64:1.14.8" \
-	[7]="k8s-dns-kube-dns-amd64:1.14.8" \
-	[8]="k8s-dns-sidecar-amd64:1.14.8" \
-	[9]="kubernetes-dashboard-amd64:v1.8.3")
+file="images.properties"
 
-for image in ${images[@]}
-do
-    echo "pull ${image} start..."
-    docker pull lsw1991abc/${image}
-    docker tag lsw1991abc/${image} k8s.gcr.io/${image}
+if [ -f "$file" ]
+then
+  echo "$file found."
+
+  while IFS='=' read -r key value version
+  do
+    #echo "${key}=${value}"
+    echo "pull ${value}:${version} start..."
+    docker pull ${value}:${version}
+    docker tag ${value}:${version} ${key}:${version}
     # 开发使用，暂不清理。避免重复下载浪费时间
-    # docker rmi lsw1991abc/${image}
-    echo "pull ${image} complete!"
-done
+    # docker rmi ${value}
+    echo "pull ${value}:${version} complete!"
+  done < "$file"
+else
+  echo "$file not found."
+fi
 
 echo "all complete!"
